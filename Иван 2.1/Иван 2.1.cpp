e0cd3579e5926a65a4f6cba54c03ee8dbd2b7ce0
sedelnikova <54905384+sedelnikova@users.noreply.github.com>
2019-09-12 21:48:19 +0300

    Добавьте файлы проекта.

---
+++ Иван 2.1/Иван 2.1.cpp
@@ -0,0 +1,21 @@
+﻿// Иван 2.1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	float x1, x2, y1, y2;
+
+	cout << "Введите координаты: ";
+
+	cin >> x1; 
+	cin >> y1; 
+	cin >> x2;
+	cin >> y2;
+	cout << sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+	return 0;
+}
